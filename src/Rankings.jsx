a76d34da054574a3cbc6f91619940b9eb59641cb
Rahul Hegde <rgh9883@rit.edu>
2024-10-13 00:38:16 -0400

    implemented sortable rankings

--- src/Rankings.jsx
+++ src/Rankings.jsx
@@ -1,9 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 function Rankings({data}){
-
     const headers = Object.keys(data[0]);
+    const [sortedBy, setSortedBy] = useState("Team");
+    const [sortedData, setSortedData] = useState(data);
+    function handleSortData(header){
+        setSortedBy(header);
+    }
+
+    useEffect(() => {
+        setSortedData(() => [...sortedData].sort((a,b) => {
+            switch(sortedBy){
+                case "Name":
+                    return a[sortedBy].localeCompare(b[sortedBy]);
+                
+                case "Team":
+                    if(a[sortedBy].localeCompare(b[sortedBy]) == 0){
+                        return a["Div"].localeCompare(b["Div"]) == 0 ? Number(b["Total"]) - Number(a["Total"]) : a["Div"].localeCompare(b["Div"]);
+                    }
+                    return a[sortedBy].localeCompare(b[sortedBy]);
+
+                case "Div":
+                    if(a[sortedBy].localeCompare(b[sortedBy]) == 0){
+                        return Number(b["Total"]) - Number(a["Total"]);
+                    }
+                    return a[sortedBy].localeCompare(b[sortedBy]);
+                
+                default:
+                    return Number(b[sortedBy]) - Number(a[sortedBy]);
+
+            }
+        }));
+    }, [sortedBy]);
+    
 
     return(
         <div>
@@ -12,12 +42,12 @@ function Rankings({data}){
             <thead>
             <tr>
                 {headers.map((header, index) => (
-                <th key={index}>{header}</th>
+                <th key={index} onClick={() => handleSortData(header)}>{header}</th>
                 ))}
             </tr>
             </thead>
             <tbody>
-            {data.map((person, index) => (
+            {sortedData.map((person, index) => (
                 <tr key={index}>
                 {headers.map((header, index) => (
                     <td key={index}>{person[header]}</td>
