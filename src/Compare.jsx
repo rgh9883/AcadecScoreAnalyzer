ddd777994e90d83a9e552c1bb170ab21777ba2ae
Rahul Hegde <rgh9883@rit.edu>
2024-10-13 02:54:12 -0400

    more work on compare feature

--- src/Compare.jsx
+++ src/Compare.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Select from "react-select";
 
 
@@ -14,24 +14,45 @@ function Compare({data}){
     const handleSelectPerson = (person, index) => {
         let p = persons;
         p[index] = person;
-        setPersons(p)
+        setPersons(p);
+        setShow(false);
     }
 
-    const showComparison = () => {
-        if(!persons.includes(null)){
-            setShow(!show);
-        }
-        else{
-            setShow(false);
-        }
-    }
 
 
     return(
         <div>
             <Select className="basic-single" classNamePrefix="select" defaultValue={persons[0]} isSearchable={true} name="personOne" options={personNames} onChange={(person) => handleSelectPerson(person, 0)}/>
             <Select className="basic-single" classNamePrefix="select" defaultValue={persons[1]} isSearchable={true} name="personOne" options={personNames} onChange={(person) => handleSelectPerson(person, 1)}/>
-            <button onClick={() => showComparison}>Compare Scores</button>
+            <button onClick={() => setShow(!show)}>Compare Scores</button>
+            {show && 
+                <table>
+                    <thead>
+                        <tr>
+                            {headers.map((header, index) => (
+                                <th key={index}>{header}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {persons.map((person, index) => (
+                        <tr key={index}>
+                            {headers.map((header, index) => (
+                                    <td key={index}>{person.value[header]}</td>
+                            ))}
+                        </tr>
+                    ))}
+                    <tr>{
+                        headers.map((header, index) => (
+                        <td key={index}>
+                            {
+                                header !== "Name" ? ["Team", "Div"].includes(header) ? "N/A" : Number(persons[0].value[header]) - Number(persons[1].value[header]) : "Difference"
+                            }
+                        </td>))
+                    }</tr>
+                    </tbody>
+                </table>
+            }
         </div>
     )
 }
